6c4675d6a6551823d8d8a957fd2d2173fb8b2eed
mari <maricrisbonzo@gmail.com>
2017-10-25 17:46:07 -0700

    changes to #20

--- solutions/20.js
+++ solutions/20.js
@@ -1,7 +1,19 @@
+<<<<<<< HEAD
 const solution = (levels = []) => {
   if (!levels.length) return;
   const n = levels.shift();
   solution(levels.concat(n.children));
+=======
+const solution = (current, next = [], result = []) => {
+  if (current.length == 0 && next.length == 0) return result;
+  if (current.length == 0) {
+    result.push(next);
+    const n = current.shift();
+    current = next;
+    next = [];
+  }
+  return solution(current, next.concat(n.children), result);
+>>>>>>> 3f17e2e... changes to #20
 };
 
 module.exports = solution;
