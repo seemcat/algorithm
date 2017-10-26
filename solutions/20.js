728c440981538736452d6723d2a53c7e0f9c9f94
mari <maricrisbonzo@gmail.com>
2017-10-25 19:16:28 -0700

    code that actually works

--- solutions/20.js
+++ solutions/20.js
@@ -1,19 +1,12 @@
-<<<<<<< HEAD
-const solution = (levels = []) => {
-  if (!levels.length) return;
-  const n = levels.shift();
-  solution(levels.concat(n.children));
-=======
-const solution = (current, next = [], result = []) => {
+const solution = (current, next = [], result = [current.slice()]) => {
   if (current.length == 0 && next.length == 0) return result;
   if (current.length == 0) {
-    result.push(next);
-    const n = current.shift();
-    current = next;
-    next = [];
+    result.push(next.slice());
+    return solution(next, [], result);
+  } else {
+    n = current.shift();
+    return solution(current, next.concat(n.children), result);
   }
-  return solution(current, next.concat(n.children), result);
->>>>>>> 3f17e2e... changes to #20
 };
 
 module.exports = solution;
