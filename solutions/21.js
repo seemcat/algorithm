521217f3308ff832bb326fd722dc9f34264275d2
mari <maricrisbonzo@gmail.com>
2017-10-26 11:24:14 -0700

    made changes suggested by eman

--- solutions/21.js
+++ solutions/21.js
@@ -1,5 +1,5 @@
 const solution = (node, nextNode = node.next, max = node.next) => {
-  if (nextNode == null) return max;
+  if (nextNode == null || nextNode == node) return max;
   if (node.value < nextNode.value) max = nextNode.value;
   return solution(node, nextNode.next, max);
 };
