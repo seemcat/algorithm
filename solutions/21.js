aeee8fce69c2110ff173bc4f8a2448a8710d3b71
mari <maricrisbonzo@gmail.com>
2017-10-30 11:40:34 -0700

    changes suggested by Alberto

--- solutions/21.js
+++ solutions/21.js
@@ -1,4 +1,4 @@
-const solution = (node, nextNode = node.next, max = node.next) => {
+const solution = (node, nextNode = node.next, max = node.value) => {
   if (nextNode == null || nextNode == node) return max;
   if (node.value < nextNode.value) max = nextNode.value;
   return solution(node, nextNode.next, max);
--- test/21.js
+++ test/21.js
@@ -12,3 +12,14 @@ const node3 = {value: 43, next: null};
 const node2 = {value: 5, next: node3};
 const node1 = {value: 19, next: node2};
 test(node1, 43);
+
+const node3a = {value: -43, next: null};
+const node2a = {value: 5, next: node3a};
+const node1a = {value: -19, next: node2a};
+test(node1a, 5);
+
+const node3b = {value: -43, next: null};
+const node2b = {value: -5, next: node3b};
+const node1b = {value: -19, next: node2b};
+test(node1b, -5);
+
