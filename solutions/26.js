5897aa3acab01c2dff0c92e4d7f9c7e1900bc9ef
mari <maricrisbonzo@gmail.com>
2017-11-01 13:50:34 -0700

    made changes suggested by song

--- solutions/26.js
+++ solutions/26.js
@@ -1,6 +1,5 @@
 const solution = (arr, n1, j = 0, ans) => {
   if (arr.length == j) return ans;
-  const n2 = Math.abs(n1 - arr[j]);
 
   const isExist = (array, n1, i = 0, result) => {
     if (array.length == i) return result;
@@ -9,8 +8,7 @@ const solution = (arr, n1, j = 0, ans) => {
     return isExist(array, n1, i + 1, result);
   };
 
-  if (isExist(arr, n2) == true) ans = true;
-  else ans = false;
+  ans = isExist(arr, Math.abs(n1 - arr[j]));
   return solution(arr, n1, j + 1, ans);
 };
 
